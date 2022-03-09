e0664c01fc8cd5f030904fddf02aeae61219602d
unknown <ibrahimkhalil.mine@gmail.com>
2022-03-09 06:44:53 +0600

    done

--- shoping.js
+++ shoping.js
@@ -1,19 +1,28 @@
 const addToList = () => {
     const input = document.getElementById("item-input")
+    const name = input.value
     const ul = document.getElementById("item-list")
     const li = document.createElement("li")
     li.innerHTML = input.value
     ul.appendChild(li)
-    curt()
+    addTocart(name)
 
 }
 
-const curt = () => {
-    const shopingCurt = localStorage.getItem("Curt")
+const cart = () => {
+    const shopingCart = localStorage.getItem("cart")
     let cartItem;
-    if (shopingCurt) {
-        localStorage.setItem(cartItem)
+    if (shopingCart) {
+        cartItem = JSON.parse(cart)
     } else {
         cartItem = {}
     }
+    return cartItem
+}
+
+const addTocart = name => {
+    const add = cart()
+    add[name] = 1
+    const product = JSON.stringify(add)
+    localStorage.setItem("cart", product)
 }
